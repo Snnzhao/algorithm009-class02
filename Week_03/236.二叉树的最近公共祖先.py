f3fd55bff81c9c2b62cddc29b8cd80fc86e1f796
snnzhao <szhao44@jh.edu>
2020-06-08 00:27:57 +0800

    week3 assignment

---
+++ Week_03/236.二叉树的最近公共祖先.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=236 lang=python3
+#
+# [236] 二叉树的最近公共祖先
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None:
+            return None
+        
+        left_res = self.lowestCommonAncestor(root.left, p, q)
+        right_res = self.lowestCommonAncestor(root.right, p, q)
+        
+        if (left_res and right_res) or (root in [p, q]):
+            return root
+        else:
+            return left_res or right_res
+# @lc code=end
+
--- Week_03/README.md
+++ Week_03/README.md
@@ -1,1 +1,7 @@
 学习笔记
+递归 Recursion
+	递归结构： recursion reminator、process logical in current level
+	           drill down 、reverse the  current level status if needed
+	递归要点：1不要人肉递归 2找最近最简单子方法 3数学归纳法思维
+	
+	爬楼梯问题：
