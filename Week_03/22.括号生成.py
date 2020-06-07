994c3cdf51adec0047598041189adcac266281e4
snnzhao <szhao44@jh.edu>
2020-06-08 00:30:57 +0800

    week3 assignment

---
+++ Week_03/22.括号生成.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode.cn id=22 lang=python3
+#
+# [22] 括号生成
+#
+
+# @lc code=start
+class Solution:
+    rst=[]
+    def generateParenthesis(self, n: int):
+        self.rst=[]
+        self._generate(0,0,n,'')
+        return self.rst
+
+    def _generate(self, left: int, right: int, max: int, strng: str):
+        #terminal
+        if left==right and left==max:
+            self.rst.append(strng)
+            return
+        #process
+        #drill down
+        if left<max:
+            self._generate(left+1,right,max,strng+'(')
+        if right<left:
+            self._generate(left,right+1,max,strng+')')
+        
+        #reverse
+        return
+
+# @lc code=end
+
--- Week_03/README.md
+++ Week_03/README.md
@@ -4,4 +4,17 @@
 	           drill down 、reverse the  current level status if needed
 	递归要点：1不要人肉递归 2找最近最简单子方法 3数学归纳法思维
 	
+	递归模板：
+	const recursion = function(level, param1, param2, ...) {
+    if (level > MAX_LEVEL) {
+         // process result 
+         return; 
+    }
+    // process current logic 
+    process(level, param);
+
+  // drill down 
+  recur( level: level + 1, newParam); 
+  // restore current status 
+  }
 	爬楼梯问题：
